06b3b740e4df6714d396769b32d1057142d2205c
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-10-02 05:53:47 -0700

    median

--- Median.py
+++ Median.py
@@ -3,30 +3,37 @@
 """we are in search of the median of two sorted arrays
 so we first merge them to one array to get one array first"""
 
-def median(arr1, arr2)
-new_arr = []
-i = j = 0
-middle
+def find_median(arr1, arr2):
+    new_arr = []
+    i = j = 0
 
-while i < len(arr1) and j < len(arr2):
-    if arr1[i] < arr2[j]:
-	new_arr.append(arr1[i])
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            new_arr.append(arr1[i])
             i += 1
-    else:
-        new_arr.append(arr2[j])
+        else:
+            new_arr.append(arr2[j])
             j += 1
 
-"""incase there are others"""
-new_arr.extend[i:]
-new_arr.extend[j:]
+    """incase there are others"""
+    new_arr.extend(arr1[i:])
+    new_arr.extend(arr2[j:])
 
-"""total lenght"""
-total = len(new_arr)
-middle = total // 2
-if total % 2 == 0:
-    median = (new_arr[middle] + new_arr[middle - 1]) / 2
-    """check for the median"""
-else:
-    median = (new_arr[middle])
+    """total lenght"""
+    total = len(new_arr)
+    middle = total // 2
 
-return median
+    if total % 2 == 0:
+        median = (new_arr[middle] + new_arr[middle - 1]) / 2
+        """check for the median"""
+    else:
+        median = (new_arr[middle])
+
+    return median
+
+
+#lets check if it works
+arr1 = [1, 3]
+arr2 = [2, 4, 6]
+result = find_median(arr1, arr2)
+print(result)
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This is me practicing interview questions. To get better.
 1. Find the median of two sorted arrays.
+    tips:
+    find the length of the two arrays indidviually, merge them into one by using the bubble sort method
+    check for the length of the array, get the middle which is the lenght //2 and return it as the median.
+    Where the array is even number, that is len % 2 == 0, then you need the middle number and the left most number, which is middle -1, and get the average. This is the logic. Now, unto the next question.
 
